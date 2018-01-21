01fce029a3a64e3f71a6986fe0690895fb2c4139
Wolfgang Zolleis <wzolleis@web.de>
2018-01-21 22:20:14 +0100

    Repo testweise noch ohne Zugriff auf die DB

--- src/main/kotlin/de/wz/muckibude/exercises/ExerciseRepo.kt
+++ src/main/kotlin/de/wz/muckibude/exercises/ExerciseRepo.kt
@@ -1,17 +1,20 @@
 package de.wz.muckibude.exercises;
 
-import org.jetbrains.exposed.sql.transactions.transaction
-
 class ExerciseRepo() {
 
-    fun findAll() : Set<Exercise> {
-        return transaction {
-            // DSL/DAO operations go here
-            val ex1 = Exercise("1", "A")
+    fun findAll(): Set<Exercise> {
+//        return transaction {
+//            // DSL/DAO operations go here
+//            val ex1 = Exercise("1", "A")
+//            val ex2 = Exercise("2", "B")
+//            println("default daten")
+//            setOf(ex1, ex2)
+//        }
+
+        val ex1 = Exercise("1", "A")
             val ex2 = Exercise("2", "B")
             println("default daten")
-            setOf(ex1, ex2)
-        }
+            return setOf(ex1, ex2)
     }
 
 }
