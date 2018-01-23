017ddd0b1eb923d86c170532c6962baf70a3b488
Wolfgang Zolleis <wzolleis@web.de>
2018-01-24 00:54:36 +0100

    Laden der Daten in einer Transaction

--- src/main/kotlin/de/wz/muckibude/exercises/FindExerciseUseCase.kt
+++ src/main/kotlin/de/wz/muckibude/exercises/FindExerciseUseCase.kt
@@ -7,8 +7,6 @@ class FindExerciseUseCase(val repo : ExerciseRepo) {
     }
 
     fun findAll() : Set<Exercise> {
-        val ex1 = Exercise("1", "A")
-        val ex2 = Exercise("2", "B")
-        return setOf(ex1, ex2)
+        return repo.findAll()
     }
 }
